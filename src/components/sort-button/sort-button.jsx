a9b967f537f8eae6b00b419b178d77ee14a5f864
SaintSanta <aleks2art@gmail.com>
2023-02-25 00:27:00 +0300

    feat: sort rating button

--- src/components/sort-button/sort-button.jsx
+++ src/components/sort-button/sort-button.jsx
@@ -2,10 +2,10 @@ import sortImg from '../../assets/icon/icon_sort-ascending.png';
 
 import './sort-button.scss';
 
-export const SortButton = () => (
+export const SortButton = ({ sortRating, sortUp }) => (
   <div className='sort'>
-    <button className='sort-button' type='button'>
-      <img src={sortImg} alt='' />
+    <button className='sort-button' type='button' onClick={sortRating} data-test-id='sort-rating-button'>
+      <img className={sortUp ? 'sort-icon sort-up' : 'sort-icon'} src={sortImg} alt='' />
       <span>По рейтингу</span>
     </button>
   </div>
