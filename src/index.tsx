35ef0b7998a3eee9871a7fd1fa3b98da2452e7ea
SaintSanta <aleks2art@gmail.com>
2023-03-12 17:19:12 +0300

    feat: add routes

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,15 @@ import { Provider } from 'react-redux/es/exports';
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import { Layout } from './components/layout/layout';
+import { LayoutAuth } from './components/layout-auth';
+import { TokenCheck } from './components/token-check/token-check';
 import { RoutePath } from './models/route-path.enum';
+import { LoginPage } from './pages/auth/login-page';
 import { BookPage } from './pages/book';
 import { ContractOffer } from './pages/contract-offer';
 import { MainPage } from './pages/main';
+import { Registration } from './pages/registration';
+import { ResetPasswordPage } from './pages/reset-password';
 import { Terms } from './pages/terms';
 import { store } from './store';
 
@@ -18,17 +23,31 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <HashRouter>
-      <Routes>
-        <Route path={RoutePath.main} element={<Layout />}>
-          <Route path={RoutePath.main} element={<Navigate to={RoutePath.booksAll} />} />
-          <Route path={RoutePath.booksCategory} element={<MainPage />} />
-          <Route path={RoutePath.bookPage} element={<BookPage />} />
-          <Route path={RoutePath.terms} element={<Terms />} />
-          <Route path={RoutePath.contract} element={<ContractOffer />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+      <HashRouter>
+        <Routes>
+          <Route element={<LayoutAuth />}>
+            <Route path={RoutePath.authorization} element={<LoginPage />} />
+            <Route path={RoutePath.registration} element={<Registration />} />
+            <Route path={RoutePath.forgotPass} element={<ResetPasswordPage />} />
+          </Route>
+        </Routes>
+        <Routes>
+          <Route
+            path={RoutePath.main}
+            element={
+              <TokenCheck>
+                <Layout />
+              </TokenCheck>
+            }
+          >
+            <Route path={RoutePath.main} element={<Navigate to={RoutePath.booksAll} />} />
+            <Route path={RoutePath.booksCategory} element={<MainPage />} />
+            <Route path={RoutePath.bookPage} element={<BookPage />} />
+            <Route path={RoutePath.terms} element={<Terms />} />
+            <Route path={RoutePath.contract} element={<ContractOffer />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
